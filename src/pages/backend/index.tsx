d1b8cf14d15c369dd7d50bf1255b704451aa36db
EPAY-PRESCO\ian.fan <ian.fan@presco.ws>
2024-01-17 14:30:10 +0800

    新增前後端資料夾 / 整理無需使用的code

---
+++ src/pages/backend/index.tsx
@@ -0,0 +1,7 @@
+export function Index() {
+  return (
+    <>
+      <h1>我是後台首頁</h1>
+    </>
+  )
+}
---
+++ src/pages/frontend/index.tsx
@@ -0,0 +1,7 @@
+export function Index() {
+  return (
+    <>
+      <h1>我是前台首頁</h1>
+    </>
+  )
+}
