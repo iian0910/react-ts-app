f4d49cff36db339f33175b9c26d314f2c5aca940
EPAY-PRESCO\ian.fan <ian.fan@presco.ws>
2024-01-18 15:12:38 +0800

    Update: 建立 Input 元件

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,14 @@
+import './Input.scss'
+
+export function Input(){
+  return (
+    <>
+      <input
+        type="email"
+        className="Body form-control"
+        id="exampleFormControlInput1"
+        placeholder="name@example.com"
+      />
+    </>
+  )
+}
