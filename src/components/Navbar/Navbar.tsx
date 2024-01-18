e9c52927fca4c552218ca2f2c07f1deee9fef325
EPAY-PRESCO\ian.fan <ian.fan@presco.ws>
2024-01-18 14:37:12 +0800

    Update: Navbar 元件製作完成

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,25 +3,30 @@ import "./Navbar.scss"
 
 import { Button } from "../Button/Button"
 
-export function Navbar(){
+interface blockStatus {
+  showNavItem: boolean
+}
+
+export function Navbar({ showNavItem }: blockStatus){
   return (
     <>
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid px-0">
-          <img className="hex_logo mr-auto" src="images/hex_logo.svg" alt="" />
-          <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            {/* <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">前台畫面</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/backend">後台畫面</Link>
-              </li>
-            </ul> */}
-            <div>
-              <span className="navItem Title">客房資訊</span>
-              <span className="navItem Title">使用者</span>
-              <Button text='登入' status='text'/>
+          <img
+            className="hex_logo mr-auto"
+            src="images/hex_logo.svg"
+            alt=""
+          />
+          <div className={showNavItem ? 'd-block' : 'd-none'}>
+            <div
+              className="collapse navbar-collapse justify-content-end"
+              id="navbarSupportedContent"
+            >
+              <div className="d-flex align-items-center">
+                <span className="navItem Title me-3">客房資訊</span>
+                <span className="navItem Title me-3">使用者</span>
+                <Button text='登入' status='primary'/>
+              </div>
             </div>
           </div>
         </div>
